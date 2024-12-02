9348f34e5aa9a255fa4893e94c547ec48694a609
Your Name <your.email@example.com>
2024-12-01 16:19:03 -0800

    add border raidus to bottom sheet

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app1/views/widgets/add_note_bottom_sheet.dart';
 import 'package:notes_app1/views/widgets/notes_view_body.dart';
 
 class NotesView extends StatelessWidget {
@@ -10,23 +11,17 @@ class NotesView extends StatelessWidget {
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           showModalBottomSheet(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(16),
+              ),
               context: context,
               builder: (context) {
-                return Container();
+                return const AddNoteBottomSheet();
               });
         },
         child: Icon(Icons.add),
       ),
       body: const NotesViewBody(),
     );
-  }
-}
-
-class AddNoteBottomSheet extends StatelessWidget {
-  const AddNoteBottomSheet({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container();
   }
 }
